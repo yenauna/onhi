a6736735ce03c04b72f7d86cb3aec7de05eb20bd
yenauna <yenauna@example.com>
2026-02-09 23:00:45 +0900

    supabase client 추가

---
+++ js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://ytouzrchngqyuqafziad.supabase.co",
+  "sb_publishable_WUrSn1U6sszhHrpEPOXwCA_ZQoJo6nW"
+);
